9d953c183d9aa0b523f72f6542e8be0b6f299938
daanyaal1312 <122629120+daanyaal1312@users.noreply.github.com>
2023-03-17 00:01:19 -0700

    Add files via upload

---
+++ list.py
@@ -0,0 +1,6 @@
+list.py> ...
+lsit1=[0,9,7,5,3,1]
+print(List1)
+
+list2=[[1,2],4,5]
+print[list2]
---
+++ list1.py
@@ -0,0 +1,9 @@
+list1=[0,9,7,5,3,1]
+print(list1)
+
+list2=[[1,2],4,5]
+print(list2)
+
+
+list4=[(1,2), {3:25, 4:"keqin-Li"}, 1, 2]
+print(list4)
